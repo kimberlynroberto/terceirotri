c36730529d16c2ef543b73265cda04507da9c67c
kimberlynroberto <144706907+kimberlynroberto@users.noreply.github.com>
2023-10-18 12:25:58 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function tocaSomPom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
+}
+const listaDeTeclas=document.querySelectorAll('.tecla');
+
+let contador = 0
+//Estrutura de repetição - Enquanto
+while(contador < listaDeTeclas.length) {
+listaDeTeclas[contador].onclick= tocaSom;
+tocaSomPom;('#som_tecla_splash')
+}
+contador = contador + 1;
+}
+console = log(contador); 
+
