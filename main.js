a1a7fdf907a4b848629d36ccf83f0875fc56f287
kimberlynroberto <144706907+kimberlynroberto@users.noreply.github.com>
2023-10-25 12:16:44 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -6,10 +6,11 @@ const listaDeTeclas=document.querySelectorAll('.tecla');
 let contador = 0
 //Estrutura de repetição - Enquanto
 while(contador < listaDeTeclas.length) {
-listaDeTeclas[contador].onclick= tocaSom;
-tocaSomPom;('#som_tecla_splash')
-}
+    listaDeTeclas[contador].classList[1];
+listaDeTeclas[contador].onclick= function(){
+tocaSom('#som_tecla_splash');
+};
 contador = contador + 1;
 }
-console = log(contador); 
+console = 
 
